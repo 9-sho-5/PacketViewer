9c88337711574aebe5231a34b866a97e30375ae7
9-sho-5 <hosho.kusu@icloud.com>
2022-11-22 19:16:08 +0900

    fix: set default data

--- app.rb
+++ app.rb
@@ -3,5 +3,21 @@ Bundler.require
 require 'sinatra/reloader' if development?
 
 get '/' do
-  erb :index
+    @data = {
+        version: 0,
+        length: 0,
+        service_type: 0,
+        packet_length: 0,
+        identificatipn: 0,
+        flagment: 0,
+        ttl: 0,
+        transport: 0,
+        header_checksum: 0,
+        source_ip_address: 0,
+        destination_ip_address: 0,
+        options: 0,
+        padding: 0,
+        data: 0,
+    }
+    erb :index
 end
