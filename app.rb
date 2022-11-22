cfe032b3c3300f5a668b3f1d3e96af0f189ca944
9-sho-5 <hosho.kusu@icloud.com>
2022-11-22 20:20:30 +0900

    fix: null value handling

--- app.rb
+++ app.rb
@@ -25,19 +25,19 @@ end
 post '/set_data' do
     data = params["data"]
     @res = {
-        version: data[0, 1],
-        length: data[1, 1],
-        service_type: data[2, 2],
-        packet_length: data[4, 4],
-        identificatipn: data[8, 4],
-        flagment: data[12, 4],
-        ttl: data[16, 2],
-        transport: data[18, 2],
-        header_checksum: data[20, 4],
-        source_ip_address: data[24, 8],
-        destination_ip_address: data[32, 8],
+        version: data[0, 1] != "" ? data[0, 1] : "null",
+        length: data[1, 1] != "" ? data[1, 1] : "null",
+        service_type: data[2, 2] != "" ? data[2, 2] : "null",
+        packet_length: data[4, 4] != "" ? data[4, 4] : "null",
+        identificatipn: data[8, 4] != "" ? data[8, 4] : "null",
+        flagment: data[12, 4] != "" ? data[12, 4] : "null",
+        ttl: data[16, 2] != "" ? data[16, 2] : "null",
+        transport: data[18, 2] != "" ? data[18, 2] : "null",
+        header_checksum: data[20, 4] != "" ? data[20, 4] : "null",
+        source_ip_address: data[24, 8] != "" ? data[24, 8] : "null",
+        destination_ip_address: data[32, 8] != "" ? data[32, 8] : "null",
         options: "未対応",
-        data: data[40..-1],
+        data: data[40..-1] != "" ? data[40..-1] : "null",
     }
     @input = data
     erb :index
